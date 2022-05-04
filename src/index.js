ac63d86506d2b2fbdaa2673f0ea42cdbcb40ce2c
SergiyRybin <serhiobax@gmail.com>
2022-05-04 20:12:11 +0300

    upd spiner

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './sass/main.scss'
 import './js/api/api-service'
 import './js/base/start-page'
 import './js/base/header-search'
-
+import './js/base/spiner'
---
+++ src/js/base/spiner.js
@@ -0,0 +1,4 @@
+// window.addEventListener("load", () => {
+//   document.querySelector(".preloader").style.display = "none"
+//   document.body.classList.add(".loaded_hiding");
+// });
