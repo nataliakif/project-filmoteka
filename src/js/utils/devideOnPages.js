2398aa86140f00e276385ac191585309c683ad06
SergiyRybin <serhiobax@gmail.com>
2022-05-05 21:36:57 +0300

    Update devideOnPages.js

--- src/js/utils/devideOnPages.js
+++ src/js/utils/devideOnPages.js
@@ -1,7 +1,21 @@
-function divideOnPages(arrayOfId, perPageAmount) {
-  //функция которая принимает массив id (в частности из localStorage)
-  //и возвращает двухмерный массив
+// -------------example--------------
+const arrayOfId = [9, 3, 6, 4, 6, 0, 6, 8, 9, 10, 11, 12, 15];
+const perPageAmount = 3;
+const newArr = [];
+
+function divideOnPages(idEl, count) {
+  idEl.map((elem, inde, arr) => {
+    while (idEl.length > 0) {
+      newArr.push(arr.splice(0, count));
+    }
+    return newArr;
+  });
 }
+divideOnPages(arrayOfId, perPageAmount);
+
+//функция которая принимает массив id (в частности из localStorage)
+//и возвращает двухмерный массив
+
 //для примера
 //[1,2,3,4,5,6,7,8,9,10,11,12] - arrayOfId, perPageAmount - 4    - входящие параметры
 
