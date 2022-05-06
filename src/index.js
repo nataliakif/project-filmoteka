a38e27d55135c2b5e6d8b77a9411a5864574f7dd
OliaChuzhdiuk <90050382+OliaChuzhdiuk@users.noreply.github.com>
2022-05-06 15:57:39 +0300

    footer-modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-
 import './sass/main.scss';
 import { readState, writeState } from './js/base/state';
 import './js/api/api-service'
 import './js/base/start-page'
 import './js/base/header-search'
 import './js/base/spiner'
-
+import './js/templates/footer-modal'
