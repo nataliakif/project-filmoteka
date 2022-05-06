35c076f29c98174cf06b5a93962f9b76cc2039ce
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-05-06 13:46:58 +0300

    adding:
    adding scrollToTop:
    1. scrollToTop.js
    2. scrollToTop.html
    3. _scrollToTop.scss

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 import './sass/main.scss';
 import { readState, writeState } from './js/base/state';
-import './js/api/api-service'
-import './js/base/spiner'
-import './js/base/handlers.js'
-import './js/base/state'
-import './js/base/listeners'
-import {updateInterface} from './js/base/update'
+import './js/api/api-service';
+import './js/base/spiner';
+import './js/base/handlers.js';
+import './js/base/state';
+import './js/base/listeners';
+import { updateInterface } from './js/base/update';
+import './js/base/scrollToTop';
 
 updateInterface();
-
