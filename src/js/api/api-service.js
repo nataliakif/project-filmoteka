7a38ee0eb3094447bb865a58bd6283abb5bc5e35
MishynMykhailo <misham18092001@gmail.com>
2022-05-06 15:25:10 +0300

    Конкретно изменили всё
    Конкретно изменили всё

--- src/js/api/api-service.js
+++ src/js/api/api-service.js
@@ -1,45 +1,45 @@
 import axios from 'axios';
-const BASE_URL ='https://api.themoviedb.org/3/';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '03b99d578651e9a3ce5c6cc1a058aad2';
- 
-    async function getBySearchQuery(query, page){
-        const response = await axios.get(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${query}&page=${page}`)
-        // .then(response => {
-        //   return getGenres().then(genresList => {
-        //     return response.data.results.map( film => ({
-        //       ...film,
-        //       genres: film.genre_ids
-        //       .map(id => genresList.filter(el => el.id === id))
-        //       .flat().map(el=> el.name)         
-        //     }));
-        //   });    
-        //   });
-         
-          return response;
-        } 
-    async function getPopularFilms(page){
-        const response = axios.get(`${BASE_URL}trending/movie/week?api_key=${API_KEY}&page=${page}`)
-        // .then(response => {   
-        // return getGenres().then(genresList => {
-        //   return response.data.results.map( film => ({
-        //     ...film,
-        //     genres: film.genre_ids
-        //     .map(id => genresList.filter(item => item.id === id))
-        //     .flat().map(item=> item.name)         
-        //   }));
-        // })}    
-        // );
-        return response;
-      }
-    async function getGenres(){
-        const response = await axios.get(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`)
-        return response.data;
-    }
-    async function getFilmById(id){
-      const response = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
-      return response;
-    }
+
+async function getBySearchQuery(query, page) {
+  const response = await axios.get(
+    `${BASE_URL}search/movie?api_key=${API_KEY}&query=${query}&page=${page}`,
+  );
+  // .then(response => {
+  //   return getGenres().then(genresList => {
+  //     return response.data.results.map( film => ({
+  //       ...film,
+  //       genres: film.genre_ids
+  //       .map(id => genresList.filter(el => el.id === id))
+  //       .flat().map(el=> el.name)
+  //     }));
+  //   });
+  //   });
+
+  return response;
+}
+async function getPopularFilms(page) {
+  const response = axios.get(`${BASE_URL}trending/movie/week?api_key=${API_KEY}&page=${page}`);
+  // .then(response => {
+  // return getGenres().then(genresList => {
+  //   return response.data.results.map( film => ({
+  //     ...film,
+  //     genres: film.genre_ids
+  //     .map(id => genresList.filter(item => item.id === id))
+  //     .flat().map(item=> item.name)
+  //   }));
+  // })}
+  // );
+  return response;
+}
+async function getGenres() {
+  const response = await axios.get(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`);
+  return response.data;
+}
+async function getFilmById(id) {
+  const response = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  return response;
+}
 
 export { getBySearchQuery, getPopularFilms, getFilmById, getGenres };
-  
-
--- src/js/base/listeners.js
+++ src/js/base/listeners.js
@@ -1,6 +1,44 @@
-import {refs} from '../references/refs'
-import {onFormSubmit} from './handlers'
+import { refs } from '../references/refs';
+import {
+  homeLinkClick,
+  libTypeQueueBtnClick,
+  libTypeWatchedBtnClick,
+  myLibLinkClick,
+  onFormSubmit,
+} from './handlers';
 
-refs.searchForm.addEventListener('submit', onFormSubmit);
+refs.homePageLink.addEventListener('click', homeLinkClick);
+refs.myLibPageLink.addEventListener('click', myLibLinkClick);
+refs.headerLogoLink.addEventListener('click', homeLinkClick);
+// refs.searchForm.addEventListener('submit', onFormSubmit);
 
+function addFormListenerHome() {
+  // console.log(refs.searchForm)
+  refs.searchForm[0].addEventListener('submit', onFormSubmit);
+}
 
+function removeFormListenerHome() {
+  if (!refs.searchForm[0]) {
+    return;
+  }
+  refs.searchForm[0].removeEventListener('submit', onFormSubmit);
+}
+
+function addBtnHeaderListener() {
+  refs.watchedBtn[0].addEventListener('click', libTypeWatchedBtnClick);
+  refs.queueBtn[0].addEventListener('click', libTypeQueueBtnClick);
+}
+
+function removeBtnHeaderListener() {
+  if (!refs.watchedBtn[0] || !refs.queueBtn[0]) {
+    return;
+  }
+  refs.watchedBtn[0].removeEventListener('click', libTypeWatchedBtnClick);
+  refs.queueBtn[0].removeEventListener('click', libTypeQueueBtnClick);
+}
+export {
+  addFormListenerHome,
+  removeFormListenerHome,
+  addBtnHeaderListener,
+  removeBtnHeaderListener,
+};
