5e1227e47a82fcabb3c307585d724e0e4d415d55
MishynMykhailo <misham18092001@gmail.com>
2022-05-08 15:07:13 +0300

    add choose genre

--- src/js/templates/markupHeader.js
+++ src/js/templates/markupHeader.js
@@ -1,7 +1,10 @@
 const homeMarkup = `<form class="header-form js-header-form">
 <input class="header-form__input" placeholder="Search films" type="input" name="input" autocomplete="off">
-<button class="header-form__btn"></button>
-</form>`;
+<button class="header-form__btn"></button></form>
+<div class="btn-genre">
+    <button class="btn-genre__choose" name="choose-genre">CHOOSE BY GENRE</button>
+    <span class="btn-genre__arrow"></span>
+    </div>`;
 const myLibMarkup = `<div class="header-filter">
 <button class="header-filter__btn" name="watched-btn">WATCHED</button>
 <button class="header-filter__btn" name="queue-btn">QUEUE</button>
