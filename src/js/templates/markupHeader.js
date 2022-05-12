66133150c4cfca677fc2e56b034dd4055f7b645e
nataliakif <nataliakif@gmail.com>
2022-05-12 20:53:40 +0300

    проверяй Сережа, проверяй

--- src/js/templates/markupHeader.js
+++ src/js/templates/markupHeader.js
@@ -3,18 +3,17 @@ const homeMarkupWithGenres = `<form class="header-form js-header-form">
 <button class="header-form__btn"></button>
 </form>
 <div class="btn-genre" >
-    <button class="btn-genre__choose" name="chooseGenre">CHOOSE BY GENRE</button>
-    <span class="btn-genre__arrow"></span>
+    <button class="btn-genre__choose" name="chooseGenre">CHOOSE BY GENRE <span class="btn-genre__arrow"></span></button>
 </div>`;
 
 const homeMarkup = `<form class="header-form js-header-form">
 <input class="header-form__input" placeholder="Search films" type="input" name="input" autocomplete="off">
 <button class="header-form__btn"></button>
-</form>`
+</form>`;
 
 const myLibMarkup = `<div class="header-filter">
 <button class="header-filter__btn" name="watched-btn">WATCHED</button>
 <button class="header-filter__btn" name="queue-btn">QUEUE</button>
 </div>`;
 
-export { myLibMarkup, homeMarkup, homeMarkupWithGenres};
+export { myLibMarkup, homeMarkup, homeMarkupWithGenres };
