4e27dfac5a1064639fe578dcc2b2752d3800b057
nataliakif <nataliakif@gmail.com>
2022-05-14 15:29:00 +0300

    notyf error

--- src/js/render/renderGallery.js
+++ src/js/render/renderGallery.js
@@ -3,10 +3,15 @@ import { PAGE_TYPE, readState } from '../base/state';
 import images from '../../images/plug/*.png';
 import { isInteger } from 'lodash';
 import poster from '../../images/plug/noposter.jpg';
+import { Notyf } from 'notyf';
+
+const notyf = new Notyf();
+
 function renderGallery(data) {
   //функция которая принимает массив объектов, полученный от api (при чем не важно будут это тренды, либо поисковой запрос пользователя, либо бибилиотека)
   //ничего не возвращает
   if (data === null || data.length === 0) {
+    notyf.error('Search result not successful. Enter the correct movie name');
     const plugMarkup = `
       <img src="${images['noresult']}" alt="Nothing was found" width="240px" class="plug_image">
       <h2 class="plug_title">Opps! There is nothing to show you</h2>
